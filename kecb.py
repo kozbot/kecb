7487ab3835165c44558fdae69d4d8f018a96ee3f
Eric A Kozub <erickozub@gmail.com>
2017-05-24 22:54:01 -0400

    Start building Cursor class

--- kecb.py
+++ kecb.py
@@ -8,6 +8,105 @@ UNIT_SCALE = BLOCK_SIZE / UPB
 SCALE = np.array([UNIT_SCALE, UNIT_SCALE])  # [ X, Y ]
 
 
+class Cursor(object):
+    """Cursor"""
+
+    def __init__(self, layout,
+                 scale=SCALE, offset=np.array([0, 0]), UPB=UNITS_PER_BLOCK):
+
+        super(Cursor, self).__init__()
+        self.layout = layout
+        self.scale = scale
+        self.offset = offset
+        self.UPB = UPB
+
+    # Utility primitive methods for readability
+
+    def add_line(self, start, end):
+
+        self.layout.add_line(
+            (np.array([start[0], start[1]]) + self.offset) * self.scale,
+            (np.array([end[0], end[1]]) + self.offset) * self.scale
+        )
+
+    def add_circle(self, center, radius):
+
+        self.layout.add_circle(
+            (np.array([center[0], center[1]]) + self.offset) * self.scale,
+            (np.array([radius])) * UNIT_SCALE  # Radius
+        )
+
+    # Drawing methods
+
+    def draw_no_contact(self):
+
+        self.add_line((0, 0), (5, 0))
+
+        self.add_line((15, 0), (20, 0))
+
+        self.add_line((5, 10), (5, -10))
+
+        self.add_line((15, 10), (15, -10))
+
+    def draw_nc_contact(self):
+
+        self.draw_no_contact
+
+    def draw_inline_terminal(self, left=True, right=True, label=None):
+
+        self.add_circle((10, 0), 5)
+
+        if left:
+            self.add_line((0, 0), (5, 0))
+
+        if right:
+            self.add_line((15, 0), (20, 0))
+
+        if label is not None:
+            # TODO: Add
+            # self.add_text(self,label,pos,height=10,align='MIDDLE_CENTER')
+            self.layout.add_text(
+                label, dxfattribs={'height': 10 * UNIT_SCALE}
+            ).set_pos(
+                (np.array([10, -10]) + self.offset) * self.scale,
+                align='MIDDLE_CENTER'
+            )
+
+    # Chainable methods
+
+    def Right(self, num_blocks=1):
+
+        self.offset = self.offset + np.array([num_blocks * self.UPB, 0])
+
+        return self
+
+    def NO(self):
+
+        self.draw_no_contact()
+
+        self.Right(1)
+
+        return self
+
+    def NC(self):
+
+        self.draw_no_contact()
+
+        self.add_line((0, -10), (20, 10))
+
+        self.Right(1)
+
+        return self
+
+    def ITERM(self, **kwargs):
+
+        self.draw_inline_terminal(**kwargs)
+
+        self.Right(1)
+
+        return self
+
+
 def draw_multipole(method, layout, scale=SCALE, offset=np.array([0, 0]),
                    gap=2, poles=1):
     original_offset = offset
@@ -61,7 +160,8 @@ def draw_terminal(layout, scale=SCALE, offset=np.array([0, 0])):
     )
 
 
-def draw_inline_terminal(layout, scale=SCALE, offset=np.array([0, 0]), left=True, right=True, label=None):
+def draw_inline_terminal(layout, scale=SCALE, offset=np.array([0, 0]),
+                         left=True, right=True, label=None):
 
     layout.add_circle(
         (np.array([10, 0]) + offset) * scale,  # Center
@@ -199,86 +299,84 @@ if __name__ == '__main__':
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_no_contact(msp)
-    dwg.saveas('./dxf/A_NO.dxf')
+    dwg.saveas('./dxf/NO.dxf')
 
     print("A: Normally Open Contact w/ Inline Terminals")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
-    draw_inline_terminal(msp, label='#')
-    draw_no_contact(msp, offset=np.array([1 * UPB, 0]))
-    draw_inline_terminal(msp, offset=np.array([2 * UPB, 0]), label='#')
-    dwg.saveas('./dxf/A_NO_ITERM.dxf')
+    cur = Cursor(msp)
+    cur.ITERM(label='#').NO().ITERM(label='#')
+    dwg.saveas('./dxf/NO_ITERM.dxf')
 
     print("A: Normally Closed Contact")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_nc_contact(msp)
-    dwg.saveas('./dxf/A_NC.dxf')
+    dwg.saveas('./dxf/NC.dxf')
 
     print("A: Normally Closed Contact w/ Inline Terminals")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
-    draw_inline_terminal(msp, label='#')
-    draw_nc_contact(msp, offset=np.array([1 * UPB, 0]))
-    draw_inline_terminal(msp, offset=np.array([2 * UPB, 0]), label='#')
-    dwg.saveas('./dxf/A_NC_ITERM.dxf')
+    cur = Cursor(msp)
+    cur.ITERM(label='#').NC().ITERM(label='#')
+    dwg.saveas('./dxf/NC_ITERM.dxf')
 
     print("A: Terminal")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_terminal(msp)
-    dwg.saveas('./dxf/A_ETERM.dxf')
+    dwg.saveas('./dxf/ETERM.dxf')
 
     print("A: Inline Terminal")
     dwg = ezdxf.new()
-    msp = dwg.modelspace()
-    draw_inline_terminal(msp)
-    dwg.saveas('./dxf/A_ITERM.dxf')
+    cur = Cursor(msp)
+    cur.ITERM(label='#')
+    dwg.saveas('./dxf/ITERM.dxf')
 
     print("A: Circuit Breaker - 1 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_circuit_breaker(msp, poles=1)
-    dwg.saveas('./dxf/A_CB_1P.dxf')
+    dwg.saveas('./dxf/CB_1P.dxf')
 
     print("A: Circuit Breaker - 2 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_circuit_breaker(msp, poles=2)
-    dwg.saveas('./dxf/A_CB_2P.dxf')
+    dwg.saveas('./dxf/CB_2P.dxf')
 
     print("A: Circuit Breaker - 3 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_circuit_breaker(msp, poles=3)
-    dwg.saveas('./dxf/A_CB_3P.dxf')
+    dwg.saveas('./dxf/CB_3P.dxf')
 
     print("A: Thermal Overload - 1 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_thermal_overload(msp)
-    dwg.saveas('./dxf/A_OL_1P.dxf')
+    dwg.saveas('./dxf/OL_1P.dxf')
 
     print("A: Thermal Overload - 2 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_multipole(draw_thermal_overload, msp, gap=2, poles=2)
-    dwg.saveas('./dxf/A_OL_2P.dxf')
+    dwg.saveas('./dxf/OL_2P.dxf')
 
     print("A: Thermal Overload - 3 Pole")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_multipole(draw_thermal_overload, msp, gap=2, poles=3)
-    dwg.saveas('./dxf/A_OL_3P.dxf')
+    dwg.saveas('./dxf/OL_3P.dxf')
 
     print("A: Solenoid")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_solenoid(msp)
-    dwg.saveas('./dxf/A_SOL.dxf')
+    dwg.saveas('./dxf/SOL.dxf')
 
     print("A: Motor Protector")
     dwg = ezdxf.new()
     msp = dwg.modelspace()
     draw_thermal_magnetic_breaker(msp)
-    dwg.saveas('./dxf/A_MP.dxf')
+    dwg.saveas('./dxf/MP.dxf')
