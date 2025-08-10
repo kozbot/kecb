4510854ee44803bbd9d5460c6bb36bf0a213710e
Kozbot <erickozub@gmail.com>
2025-08-09 21:05:11 -0400

    Update coil.py
    Added initial coil.

--- symlib/coil.py
+++ symlib/coil.py
@@ -12,3 +12,14 @@ class SOL(entity.CodedSymbol):
             MAGNETIC().translate(xoff=20),
             ITERM().translate(xoff=40),
         ]
+
+class COIL(entity.CodedSymbol):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def generate(self):
+        return [
+            ITERM(),
+            entity.Circle(center=entity.Point(20, 0),radius=20).translate(xoff=20, yoff=0),
+            ITERM().translate(xoff=60, yoff=0)
+        ]
